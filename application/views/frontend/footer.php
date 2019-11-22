99359ec73811717156f1048bf88a9b2017c08b92
hory12 <horyradiansyah5@gmail.com>
2019-11-22 18:54:45 +0700

    add_frontend

---
+++ application/views/frontend/footer.php
@@ -0,0 +1,23 @@
+<div class="clearfix"></div>
+        <footer>
+            <div class="container-fluid">
+                <p class="copyright">&copy; 2017 <a href="https://www.themeineed.com" target="_blank">Theme I Need</a>. All Rights Reserved.</p>
+            </div>
+        </footer>
+    </div>
+<script src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.slimscroll.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/klorofil-common.js');?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.dataTables.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/select2.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/jquery-ui.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/main.js');?>"></script>
+<script src="<?php echo base_url('assets/js/toastr.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.timepicker.min.js');?>"></script>
+<script src="<?php echo base_url('assets/js/dropzone.js');?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.validate.js');?>"></script>
+</body>
+</html>
+
+
